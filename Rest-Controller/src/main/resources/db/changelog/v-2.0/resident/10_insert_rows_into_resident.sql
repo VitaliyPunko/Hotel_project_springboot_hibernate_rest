17827e78fce5f7e7b71fa8d07edadc0682de135f
v.punko <Corvet23>
2022-04-19 16:44:33 +0300

    add liquibase for this application

---
+++ Rest-Controller/src/main/resources/db/changelog/v-2.0/resident/10_insert_rows_into_resident.sql
@@ -0,0 +1,8 @@
+Insert into resident (RESIDENT_ID, FIRSTNAME, LASTNAME, EMAIL, ARRIVAL_TIME, DEPARTURE_TIME, APARTMENT_ID)
+values(1, 'Stephen', 'King', 'stephenking@test.com', '2021-03-13', '2021-03-23', 1);
+Insert into resident (RESIDENT_ID, FIRSTNAME, LASTNAME, EMAIL, ARRIVAL_TIME, DEPARTURE_TIME, APARTMENT_ID)
+values(2, 'Margaret', 'Mitchell', 'margaretmitchell@test.com', '2020-10-26', '2021-04-10', 2);
+Insert into resident (RESIDENT_ID, FIRSTNAME, LASTNAME, EMAIL, ARRIVAL_TIME, DEPARTURE_TIME, APARTMENT_ID)
+values(3, 'Den', 'Brown', 'denbrown@test.com', '2021-02-13', '2021-02-25', 1);
+Insert into resident (RESIDENT_ID, FIRSTNAME, LASTNAME, EMAIL, ARRIVAL_TIME, DEPARTURE_TIME, APARTMENT_ID)
+values(4, 'Erich', 'Remark', 'remark@test.com', '2021-04-10', '2021-06-01', 2);
